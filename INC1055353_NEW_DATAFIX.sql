e40b66c272a6dad37a74d32cd150757dcf9445db
Vishal <101555756+Vshal321@users.noreply.github.com>
2025-12-04 19:12:12 +0530

    Add files via upload

---
+++ INC1055353_NEW_DATAFIX.sql
@@ -0,0 +1,87 @@
+UPDATE bolinf.trex_occ_order_header_stg
+SET
+    bill_to_cust_accnt_site_id = 8078503,
+    ship_to_cust_accnt_site_id = 10575333
+WHERE
+    orig_sys_document_ref = 'o11733373';
+	
+--1 row updated--
+
+UPDATE bolinf.trex_occ_order_header_stg
+SET
+    bill_to_cust_accnt_site_id = 21167908,
+    ship_to_cust_accnt_site_id = 21162888
+WHERE
+    orig_sys_document_ref = 'o11717238';
+	
+--1 row updated--
+
+UPDATE bolinf.trex_occ_order_header_stg
+SET
+    bill_to_cust_accnt_site_id = 854710,
+    ship_to_cust_accnt_site_id = 24808857
+WHERE
+    orig_sys_document_ref = 'o11718563';
+	
+--1 row updated--
+
+UPDATE bolinf.trex_occ_order_header_stg
+SET
+    bill_to_cust_accnt_site_id = 8074635,
+    ship_to_cust_accnt_site_id = 24811798
+WHERE
+    orig_sys_document_ref = 'o11718554';
+	
+--1 row updated--
+
+UPDATE bolinf.trex_occ_order_header_stg
+SET
+    bill_to_cust_accnt_site_id = 16023727,
+    ship_to_cust_accnt_site_id = 24688741
+WHERE
+    orig_sys_document_ref = 'o11708226';
+	
+--1 row updated--
+
+UPDATE bolinf.trex_occ_order_header_stg
+SET
+    bill_to_cust_accnt_site_id = 8080336,
+    ship_to_cust_accnt_site_id = 11339368
+WHERE
+    orig_sys_document_ref = 'o11707506';
+	
+--1 row updated--
+
+UPDATE bolinf.trex_occ_order_header_stg
+SET
+    bill_to_cust_accnt_site_id = 854710,
+    ship_to_cust_accnt_site_id = 24807811
+WHERE
+    orig_sys_document_ref = 'o11717224';
+	
+--1 row updated--
+
+UPDATE bolinf.trex_occ_order_header_stg
+SET
+    bill_to_cust_accnt_site_id = 854710,
+    ship_to_cust_accnt_site_id = 24809757
+WHERE
+    orig_sys_document_ref = 'o11707094';
+	
+--1 row updated--
+
+UPDATE bolinf.trex_occ_order_header_stg
+SET
+    bill_to_cust_accnt_site_id = 4693747,
+    ship_to_cust_accnt_site_id = 21007849
+WHERE
+    orig_sys_document_ref = 'o11716519';
+
+--1 row updated--
+
+UPDATE bolinf.trex_occ_order_header_stg
+SET
+    bill_to_cust_accnt_site_id = 854710,
+    ship_to_cust_accnt_site_id = 24806791
+WHERE
+    orig_sys_document_ref = 'o11705998';
